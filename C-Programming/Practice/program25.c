35aeb859325b6dee30b870dc30aebe51d92b9160
JayALokhande <jayanillokhande@gmail.com>
2026-06-21 23:33:50 +0530

    Add files via upload

---
+++ C-Programming/Practice/program25.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+    int iCnt = 0;
+
+    //ITERATION 
+
+    for(iCnt = 1; iCnt<= 4; iCnt ++)
+    {
+        printf("Jay Ganesh.... \n");
+    }
+
+    return 0;
+}
