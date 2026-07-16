36d8ee0a12b8f967aa8af9c4feeed00618b295d1
JayALokhande <jayanillokhande@gmail.com>
2026-07-16 23:21:35 +0530

    Added C-Programming/Practice/programs 123 to 130

---
+++ C-Programming/Practice/program123.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int Summation( int Arr[], int iSize)
+{
+
+    int iSum = 0 , iCnt = 0;
+     for( iCnt = 0 ; iCnt < iSize ; iCnt ++)
+    {
+        iSum = iSum + Arr[iCnt];
+        
+    }
+    return iSum;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0;
+    int iRet = 0;
+    
+
+    printf("Enter the Number of elements : \n");
+    scanf("%d,",&iLength);
+
+    Brr = (int *)malloc(sizeof(int)* iLength);
+
+    printf("Enter The Elements : \n");
+
+    for(iCnt = 0 ; iCnt < iLength ; iCnt ++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Summation(Brr , iLength);
+
+    printf("Addition of Number is : %d", iRet);
+
+    free(Brr);
+    return 0 ;
+}
