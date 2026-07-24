6a6862fd2e11931227d15bb808e031966d612c80
JayALokhande <jayanillokhande@gmail.com>
2026-07-24 15:38:38 +0530

    Pattren Printing

---
+++ Java-Programming/Practice/program227.java
@@ -0,0 +1,68 @@
+
+
+
+/*
+iRow = 4
+iCol = 4
+
+a
+b       c
+d       e       f
+g       h       i       j
+
+optimization
+
+*/
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0, j = 0 ;
+        char ch = '\0';
+        
+        // Filter for diagonal Pattern
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid parameter");
+            System.out.println("Number of Rows & Column Should Be Same ");
+            return;
+
+        }
+
+        for(i= 1 , ch = 'a'; i <= iRow; i++ )
+        {
+            for (j = 1  ; j <= i ;j++, ch++)
+            {
+                
+                System.out.print(ch+"\t");
+                    
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+public class program227
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0 , iValue2= 0;
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+
+    
+}
