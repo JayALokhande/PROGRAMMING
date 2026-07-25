0f861de88c701f7ca4f477c9f116c7ee6b998cea
JayALokhande <jayanillokhande@gmail.com>
2026-07-25 23:24:57 +0530

    String Operation

---
+++ C-Programming/Practice/program238.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char *str= "Ganesh"; 
+    int iCount = 0 ;           //char str[] = "Ganesh";
+
+    while (*str != '\0')
+    {
+        iCount++;
+        str++;
+    }
+    printf("Length of String is  :%d\n",iCount);
+
+    return 0;
+}
