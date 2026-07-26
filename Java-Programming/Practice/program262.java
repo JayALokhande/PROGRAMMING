d18dc88cb5b661152b952afc526b51c57cf0be94
JayALokhande <jayanillokhande@gmail.com>
2026-07-26 23:44:18 +0530

    String Operation

---
+++ Java-Programming/Practice/program262.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class program262
+{
+    public static void main(String A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Arr = null;                          //reference Arr is String
+        
+        System.out.println("Enter String :");
+        Arr = sobj.nextLine();
+
+        System.out.println(Arr.charAt(0));
+        System.out.println(Arr.charAt(1));
+        System.out.println(Arr.charAt(2));
+                
+    }
+    
+}
