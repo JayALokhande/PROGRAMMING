0035b528b6a50b66fb6f29c8deaa15e35884a973
JayALokhande <jayanillokhande@gmail.com>
2026-07-26 23:40:30 +0530

    String Operation

---
+++ C-Programming/Practice/program260.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class program260 
+{
+    public static void main(String A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Arr = null;                          //reference
+        
+        System.out.println("Enter String :");
+        Arr = sobj.nextLine();
+
+        System.out.println("Entered String : "+Arr);
+        
+
+
+    }
+    
+}
