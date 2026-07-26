3d2f5975b40ccf6a6423d4730914fd10142c2340
JayALokhande <jayanillokhande@gmail.com>
2026-07-26 23:44:33 +0530

    String Operation

---
+++ Java-Programming/Practice/program263.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class program263
+{
+    public static void main(String A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Arr = null;                          //reference Arr is String
+        
+        System.out.println("Enter String :");
+        Arr = sobj.nextLine();
+
+        System.out.println("Length of String is : "+Arr.length());
+        
+                
+    }
+    
+}
