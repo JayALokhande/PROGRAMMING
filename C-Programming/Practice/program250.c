989f915b5b90e6dd2fbff63fa2003ee763b5a55e
JayALokhande <jayanillokhande@gmail.com>
2026-07-26 23:37:42 +0530

    String Operation

---
+++ C-Programming/Practice/program250.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int Count(const char *str)
+{
+    int iCount =0;
+    while (*str !='\0')
+    {
+        if(*str =='a')         //finding the a fequency from the string
+        {
+            iCount++;
+        }
+        str++;
+    } 
+    return iCount;
+}
+
+int main()
+{
+    char Arr[50]={'\0'};
+    int iRet = 0;
+
+    printf("Enter String :");
+    scanf("%[^'\n]s",Arr);                   
+
+    iRet = Count(Arr);
+
+    printf("Frequency is :%d\n",iRet);
+    return 0;
+}
