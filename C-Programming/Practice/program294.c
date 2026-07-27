05ab22d588a2fae42f40944725d9512aa5d46c15
JayALokhande <jayanillokhande@gmail.com>
2026-07-27 23:25:28 +0530

    String Operation

---
+++ C-Programming/Practice/program294.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+void RevDisplay(char *str)
+{
+    char *start = NULL;
+
+    start = str;
+
+    while (*str != '\0')
+    {
+        str++;
+    }
+    str --;
+
+    while (start <= str)
+    {
+        printf("%s\n", str);
+        str --;
+    }                                      //not run complitly
+    printf("\n");
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :\n");
+    scanf("%[^\n]s",Arr);
+    
+    RevDisplay(Arr);
+
+    return 0; 
+}
