714d55fd83c1703f2e924de6935eea3e50556e69
JayALokhande <jayanillokhande@gmail.com>
2026-07-28 22:50:17 +0530

    Binary Operation

---
+++ C-Programming/Practice/program306.c
@@ -0,0 +1,26 @@
+// Accept number from user and count the number of 1 in the Binary of that number ..
+#include<stdio.h>
+
+
+int main()
+{
+    int  iNo = 0, iCount = 0, iDigit = 0;
+
+    printf("Enter the Number :");
+    scanf("%d",&iNo);
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 2;
+        if(iDigit ==1)
+        {
+            iCount++;
+        }
+        iNo = iNo / 2;
+
+    }
+    
+    printf("Number of 1 : %d",iCount);
+
+    return 0;
+}
