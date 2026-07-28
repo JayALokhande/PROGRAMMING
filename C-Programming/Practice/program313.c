40843c01882431036b9cdc7604fc6fd7febcf33b
JayALokhande <jayanillokhande@gmail.com>
2026-07-28 22:56:08 +0530

    Binary Operation

---
+++ C-Programming/Practice/program313.c
@@ -0,0 +1,27 @@
+// Accept number ..17 bit on or off
+#include<stdio.h>
+
+typedef unsigned int UINT ;         
+
+int main()
+{
+    UINT iNo = 0;
+    UINT iMask = 0x00010000;
+    UINT iAns = 0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iNo);
+
+    iAns = iNo & iMask;
+
+    if(iAns == iMask)
+    {
+        printf("17th Bit is ON ");
+    }
+    else
+    {
+        printf("17th Bit is OFF ");
+    }
+    
+    return 0;
+}
