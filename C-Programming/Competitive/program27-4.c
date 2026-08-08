75f5fe52c7996093c188c39f5414b6526eef41ce
JayALokhande <jayanillokhande@gmail.com>
2026-08-08 23:30:29 +0530

    Problem Solving

---
+++ C-Programming/Competitive/program27-4.c
@@ -0,0 +1,48 @@
+// Accept number of rows and columns from user and diplay pattern
+// Row = 6 Column = 6
+/*pattern = *       *       *       *       *       *
+            *       *                               *
+            *               *                       *
+            *                       *               *
+            *                               *       *
+            *       *       *       *       *       *
+*/
+
+#include<stdio.h>
+
+void Pattrn(int iRow , int iCol)
+{
+    int i= 0, j = 0 ;
+
+    for( i = 1 ; i <= iRow ; i++)
+    {
+        for(j = 1 ; j <= iCol ; j++)
+        {
+            if(i==1|| i==iRow || j==1 || j==iCol || i ==j)          
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+    
+}
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    printf("Enter Number Of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattrn(iValue1,iValue2);
+
+    return 0 ;
+}
