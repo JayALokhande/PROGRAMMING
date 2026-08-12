b63e923fb811336fd2213f09afa33fcf78ccb4cd
JayALokhande <jayanillokhande@gmail.com>
2026-08-12 22:28:28 +0530

    Problem Solving

---
+++ C-Programming/Competitive/program39-1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Display(int No)
+{
+    if (No == 0)
+    {
+        return;
+    }
+
+    printf("*\t");
+
+    Display(No - 1);
+    
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number :");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
